d3afd6e95fd7bfca4f5cdd12f7dc308d32937250
Semyon <125258352+SSUHOY@users.noreply.github.com>
2023-09-01 15:06:37 +0300

    install thunk to project

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,13 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from './slicers/todo'
+import thunk from "redux-thunk";
 // переменнаая стор - результат выполнения функции configureStore
 export const store = configureStore({
   // декларация редьюсеров
   reducer: {
     todo: todoReducer,
-  }
+  },
+  // добавляет thunk 
+  middleware: [thunk],
 });
